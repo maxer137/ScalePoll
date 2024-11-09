78d41f5128fbf02133209f1008b86c7278101395
Ronald van den Broek <ronald.vandenbroek@live.nl>
2024-11-09 22:14:08 +0200

    Adding closed column for questions

--- survall_backend/database/sql_database.py
+++ survall_backend/database/sql_database.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import os
 import uuid
 from sqlalchemy import create_engine, func
@@ -48,12 +49,18 @@ class SQLDatabase():
         return questions
     
     def get_related_questions(self, question:Question):
-        questions = self.session.query(Question).filter(Question.root_question_uuid == question.root_question_uuid).order_by(Question.creation_time).all()
+        questions = self.session.query(Question).filter(
+            Question.root_question_uuid == question.root_question_uuid and
+            (Question.closed == True or 
+            Question.close_time <= datetime.now(timezone.utc))
+            ).order_by(Question.creation_time).all()
 
         return questions
 
-    def get_iterated_question(self, iteration):
-        questions = self.session.query(Question).order_by(Question.creation_time).all()
+    def get_iterated_question(self, iteration, user_uuid):
+        # TODO filter already answered questions
+
+        questions = self.session.query(Question).order_by(Question.creation_time).filter(Question.closed == False and Question.close_time >= datetime.now(timezone.utc)).all()
 
         # Get the question based on the iteration and modulus of the total number of questions
         question_index = iteration % len(questions) if questions else None
@@ -66,7 +73,7 @@ class SQLDatabase():
         return questions[question_index] if question_index is not None else None
 
     def get_random_question(self):
-        return self.session.query(Question).order_by(func.random()).first()
+        return self.session.query(Question).filter(Question.closed == False and Question.close_time >= datetime.now(timezone.utc)).order_by(func.random()).first()
     
     def check_if_user_answered_question(self, user_question_pair:UserQuestionPair):
         existing_answer = self.session.query(UserQuestionPair).filter_by(
